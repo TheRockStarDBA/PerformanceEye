232970be90141f43d4f2794adb4a3fd1f3992b9b
amorelli005 <amorelli@huronconsultinggroup.com>
2016-09-26 11:59:15 -0700

    Create CreateSchemas.sql

---
+++ _Core/CreateSchemas.sql
@@ -0,0 +1,12 @@
+USE PerformanceEye 
+GO
+CREATE SCHEMA [CorePE] 
+GO
+CREATE SCHEMA [PerformanceEye]		--the "public" interface (e.g. views and such)
+GO
+CREATE SCHEMA [AutoWho]
+GO
+CREATE SCHEMA [ServerEye]
+GO
+CREATE SCHEMA [HEM] 
+GO
