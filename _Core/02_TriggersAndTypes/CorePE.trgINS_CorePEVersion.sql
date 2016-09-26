1500ea8acb8ecc734193440c94e9ca32e0a7c566
amorelli005 <amorelli@huronconsultinggroup.com>
2016-09-26 12:01:36 -0700

    Add files via upload

---
+++ _Core/02_TriggersAndTypes/CorePE.trgINS_CorePEVersion.sql
@@ -0,0 +1,25 @@
+USE PerformanceEye
+GO
+CREATE TRIGGER [CorePE].[trgINS_CorePEVersion] ON [CorePE].[Version]
+
+FOR INSERT
+AS 	BEGIN
+
+INSERT INTO CorePE.Version_History
+([Version], 
+EffectiveDate, 
+HistoryInsertDate,
+TriggerAction)
+SELECT 
+[Version], 
+EffectiveDate, 
+GETDATE(),
+'Insert'
+FROM inserted
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
---
+++ _Core/02_TriggersAndTypes/CorePE.trgUPD_CorePEVersion.sql
@@ -0,0 +1,20 @@
+USE PerformanceEye
+GO
+CREATE TRIGGER [CorePE].[trgUPD_CorePEVersion] ON [CorePE].[Version]
+
+FOR UPDATE
+AS 	BEGIN
+
+INSERT INTO CorePE.Version_History 
+([Version], 
+EffectiveDate, 
+HistoryInsertDate,
+TriggerAction)
+SELECT 
+[Version], 
+EffectiveDate, 
+getdate(),
+'Update'
+FROM inserted
+END
+GO
