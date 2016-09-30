e0ac429a2456c795bda6862c19dbd28438fa9b61
Aaron Morelli <aaronmorelli@zoho.com>
2016-09-30 12:49:09 -0700

    Update install_database_objects.ps1

--- install_database_objects.ps1
+++ install_database_objects.ps1
@@ -1,3 +1,48 @@
+#####
+# PRODUCT:    PerformanceEye  https://github.com/amorelli005/PerformanceEye
+# PROCEDURE:	install_database_objects.ps1
+#
+# AUTHOR: Aaron Morelli
+#	email@TBD.com
+#	@sqlcrossjoin
+#	sqlcrossjoin.wordpress.com					
+#
+#	PURPOSE: Install the PerformanceEye database, its schema and T-SQL objects, and SQL Agent jobs
+#  on a SQL Server instance.
+#
+#	OUTSTANDING ISSUES: None at this time.
+#
+#   CHANGE LOG:	
+#		2016-09-30	Aaron Morelli		Final code run-through and commenting
+#
+#	MIT License
+#
+#	Copyright (c) 2016 Aaron Morelli
+#
+#	Permission is hereby granted, free of charge, to any person obtaining a copy
+#	of this software and associated documentation files (the "Software"), to deal
+#	in the Software without restriction, including without limitation the rights
+#	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#	copies of the Software, and to permit persons to whom the Software is
+#	furnished to do so, subject to the following conditions:
+#
+#	The above copyright notice and this permission notice shall be included in all
+#	copies or substantial portions of the Software.
+#
+#	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#	SOFTWARE.
+
+# To Execute
+# ------------------------
+# <this should not be called directly. Call the parent PerformanceEye_Installer.ps1 
+# script instead
+#####
+
 param ( 
 [Parameter(Mandatory=$true)][string]$Server, 
 [Parameter(Mandatory=$true)][string]$Database,
